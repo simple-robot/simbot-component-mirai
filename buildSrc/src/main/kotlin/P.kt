baccb066eefa6ae76cbb80e37b24dfd0e039c4fb
ForteScarlet <ForteScarlet@163.com>
2022-02-03 00:04:06 +0800

    to 2.0

--- buildSrc/src/main/kotlin/P.kt
+++ buildSrc/src/main/kotlin/P.kt
@@ -28,7 +28,7 @@ abstract class SimbotProject {
 sealed class P : SimbotProject() {
     object Simbot {
         const val GROUP = "love.forte.simbot"
-        const val VERSION = "3.0.0.preview.1.1"
+        const val VERSION = "3.0.0.preview.2.0"
     }
 
     object Simboot {
