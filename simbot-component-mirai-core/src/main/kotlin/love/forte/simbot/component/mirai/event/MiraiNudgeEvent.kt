e966186fa2024772f00c8f2613dea744a9503a61
ForteScarlet <ForteScarlet@163.com>
2022-04-06 17:52:18 +0800

    戳一戳相关

--- simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/event/MiraiNudgeEvent.kt
+++ simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/event/MiraiNudgeEvent.kt
@@ -17,13 +17,152 @@
 
 package love.forte.simbot.component.mirai.event
 
+import kotlinx.coroutines.launch
+import love.forte.simbot.Api4J
+import love.forte.simbot.ID
+import love.forte.simbot.action.MessageReplyReceipt
+import love.forte.simbot.action.ReplySupport
+import love.forte.simbot.component.mirai.*
+import love.forte.simbot.component.mirai.message.toMessage
+import love.forte.simbot.definition.Objectives
+import love.forte.simbot.event.*
+import love.forte.simbot.message.*
+import love.forte.simbot.randomID
+import love.forte.simbot.utils.runInBlocking
+import net.mamoe.mirai.event.events.NudgeEvent
+
 /**
  * mirai中与戳一戳相关的事件。
  *
+ * 戳一戳事件也算所相对应的消息事件。
+ *
  * 戳一戳事件算作消息事件，消息中将只包含一个 nudge 对象，不存在plainText.
+ *
+ * 不会接收 `from == bot.id` 的戳一戳事件。
+ *
+ * 针对 [mirai的戳一戳事件][NudgeEvent] 中 [subject][NudgeEvent.subject]
+ * 所示的可能类型提供4个不同的 [MiraiNudgeEvent] 子类型：
+ *
+ * - `GROUP` -> [MiraiGroupNudgeEvent]
+ * - `STRANGER` -> [MiraiMemberNudgeEvent]
+ * - `FRIEND` -> [MiraiFriendNudgeEvent]
+ * - `MEMBER` -> [MiraiStrangerNudgeEvent]
  *
  * @author ForteScarlet
  */
-public interface MiraiNudgeEvent {
-    // TODO
+public interface MiraiNudgeEvent : MiraiSimbotEvent<NudgeEvent>, MessageEvent, ReplySupport {
+    override val originalEvent: NudgeEvent
+    override val messageContent: ReceivedMessageContent
+
+    /**
+     * 事件涉及的bot。
+     */
+    override val bot: MiraiBot
+
+    //// Impl apis
+    override suspend fun reply(message: Message): MessageReplyReceipt
+    override suspend fun reply(message: MessageContent): MessageReplyReceipt
+    override suspend fun reply(text: String): MessageReplyReceipt
+
+    /**
+     * 回复此目标一个戳一戳。相当于针对当前的 target 发送一个戳一戳。
+     */
+    @JvmSynthetic
+    public suspend fun replyNudge() {
+        // TODO
+    }
+
+    /**
+     * 回复此目标一个戳一戳。相当于针对当前的 target 发送一个戳一戳。
+     */
+    @Api4J
+    public fun replyNudgeBlocking() {
+        runInBlocking { replyNudge() }
+    }
+
+    /**
+     * 回复此目标一个戳一戳。相当于针对当前的 target 发送一个戳一戳。
+     */
+    @Api4J
+    public fun replyNudgeAsync() {
+        bot.launch { replyNudge() }
+    }
+
+
+    //// Impl props
+    @OptIn(Api4J::class)
+    override val source: Objectives
+    override suspend fun source(): Objectives = source
+
+    override val key: Event.Key<out MiraiNudgeEvent>
+
+    public companion object Key : BaseEventKey<MiraiNudgeEvent>(
+        "mirai.nudge", MiraiSimbotEvent, MessageEvent
+    ) {
+        override fun safeCast(value: Any): MiraiNudgeEvent? = doSafeCast(value)
+    }
+}
+
+
+/**
+ * 针对于 [戳一戳事件][MiraiNudgeEvent] 所使用的 [ReceivedMessageContent] 实现。
+ */
+@Suppress("CanBeParameter", "MemberVisibilityCanBePrivate")
+public class MiraiReceivedNudgeMessageContent(public val nudgeEvent: NudgeEvent) : ReceivedMessageContent() {
+    override val messages: Messages = nudgeEvent.toMessage().toMessages()
+    override val messageId: ID = randomID()
+}
+
+
+/**
+ * 在群中收到的戳一戳事件。
+ *
+ * 实现 [GroupMessageEvent], 但是不同于 [MiraiGroupMessageEvent].
+ *
+ */
+public interface MiraiGroupNudgeEvent : MiraiNudgeEvent, GroupMessageEvent {
+    @OptIn(Api4J::class)
+    override val source: MiraiGroup
+    override suspend fun source(): MiraiGroup = source
+
+}
+
+
+/**
+ * 在群成员处收到的戳一戳事件。
+ *
+ * 实现 [ContactMessageEvent], 但是不同于 [MiraiMemberMessageEvent].
+ */
+public interface MiraiMemberNudgeEvent : MiraiNudgeEvent, ContactMessageEvent {
+    @OptIn(Api4J::class)
+    override val source: MiraiMember
+    override suspend fun source(): MiraiMember = source
+
+}
+
+
+/**
+ * 在好友处收到的戳一戳事件。
+ *
+ * 实现 [FriendMessageEvent], 但是不同于 [MiraiFriendMessageEvent].
+ */
+public interface MiraiFriendNudgeEvent : MiraiNudgeEvent, FriendMessageEvent {
+    @OptIn(Api4J::class)
+    override val source: MiraiFriend
+    override suspend fun source(): MiraiFriend = source
+
+
+}
+
+
+/**
+ * 在陌生人处收到的戳一戳事件。
+ *
+ * 实现 [ContactMessageEvent], 但是不同于 [MiraiStrangerMessageEvent].
+ */
+public interface MiraiStrangerNudgeEvent : MiraiNudgeEvent, ContactMessageEvent {
+    @OptIn(Api4J::class)
+    override val source: MiraiStranger
+    override suspend fun source(): MiraiStranger = source
+
 }
--- simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/message/MiraiNudge.kt
+++ simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/message/MiraiNudge.kt
@@ -17,17 +17,69 @@
 
 package love.forte.simbot.component.mirai.message
 
-import kotlinx.coroutines.*
-import kotlinx.serialization.*
-import love.forte.simbot.*
-import love.forte.simbot.component.mirai.*
+import kotlinx.coroutines.currentCoroutineContext
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import love.forte.simbot.ID
+import love.forte.simbot.InternalSimbotApi
+import love.forte.simbot.LongID
+import love.forte.simbot.component.mirai.OriginalMiraiFriend
+import love.forte.simbot.component.mirai.OriginalMiraiGroup
+import love.forte.simbot.component.mirai.OriginalMiraiMember
+import love.forte.simbot.component.mirai.OriginalMiraiStranger
 import love.forte.simbot.component.mirai.event.*
-import love.forte.simbot.component.mirai.internal.*
-import love.forte.simbot.event.*
-import love.forte.simbot.message.*
+import love.forte.simbot.component.mirai.internal.InternalApi
+import love.forte.simbot.component.mirai.message.MiraiReceivedNudge.SubjectType
+import love.forte.simbot.event.EventProcessingContext
+import love.forte.simbot.message.Message
+import love.forte.simbot.message.doSafeCast
 import net.mamoe.mirai.contact.*
-import net.mamoe.mirai.event.events.*
+import net.mamoe.mirai.event.events.NudgeEvent
+import net.mamoe.mirai.event.events.StrangerEvent
 import net.mamoe.mirai.message.action.Nudge.Companion.sendNudge
+
+
+private suspend fun sendNudge(contact: Contact, target: LongID?) {
+    if (target != null) {
+        if (target.number == contact.bot.id) {
+            contact.sendNudge(contact.bot.nudge())
+            return
+        }
+
+        when (contact) {
+            is OriginalMiraiGroup -> {
+                val nudge = contact.getMemberOrFail(target.number).nudge()
+                contact.sendNudge(nudge)
+            }
+            is OriginalMiraiFriend -> contact.sendNudge(contact.nudge())
+            is OriginalMiraiMember -> contact.sendNudge(contact.nudge())
+            is OriginalMiraiStranger -> contact.sendNudge(contact.nudge())
+        }
+    } else {
+        // 没有target
+        val event = currentCoroutineContext()[EventProcessingContext]?.event
+        if (event is MiraiSimbotEvent<*>) {
+            when (val nativeEvent = event.originalEvent) {
+                is OriginalMiraiGroupMemberEvent -> nativeEvent.member.sendNudge(nativeEvent.member.nudge())
+                is OriginalMiraiGroupMessageEvent -> nativeEvent.group.sendNudge(nativeEvent.sender.nudge())
+                is OriginalMiraiGroupEvent -> nativeEvent.group.sendNudge(nativeEvent.group.bot.nudge())
+                is OriginalMiraiFriendEvent -> nativeEvent.friend.sendNudge(nativeEvent.friend.nudge())
+                is StrangerEvent -> nativeEvent.stranger.sendNudge(nativeEvent.stranger.nudge())
+            }
+        } else {
+            // 如果 contact不是群聊，戳此目标，否则戳bot自己。
+            when (contact) {
+                is Friend -> contact.sendNudge(contact.nudge())
+                is Member -> contact.sendNudge(contact.nudge())
+                is Stranger -> contact.sendNudge(contact.nudge())
+                // 发送nudge自己
+                else -> contact.sendNudge(contact.bot.nudge())
+            }
+
+        }
+    }
+}
+
 
 /**
  * 仅用于发送的 nudge 对象, 不会在接收中出现。
@@ -42,7 +94,7 @@ import net.mamoe.mirai.message.action.Nudge.Companion.sendNudge
  */
 @SerialName("mirai.nudge")
 @Serializable
-public data class MiraiNudge constructor(
+public data class MiraiNudge @JvmOverloads constructor(
     public val target: LongID? = null,
 ) : MiraiSendOnlyComputableMessage<MiraiNudge> {
     override val key: Message.Key<MiraiNudge> get() = Key
@@ -50,37 +102,7 @@ public data class MiraiNudge constructor(
     @OptIn(InternalApi::class)
     @JvmSynthetic
     override suspend fun originalMiraiMessage(contact: Contact): OriginalMiraiMessage {
-        if (target != null) {
-            if (target.number == contact.bot.id) {
-                contact.sendNudge(contact.bot.nudge())
-                return EmptySingleMessage
-            }
-
-            when (contact) {
-                is OriginalMiraiGroup -> {
-                    val nudge = contact.getMemberOrFail(target.number).nudge()
-                    contact.sendNudge(nudge)
-                }
-                is OriginalMiraiFriend -> contact.sendNudge(contact.nudge())
-                is OriginalMiraiMember -> contact.sendNudge(contact.nudge())
-                is OriginalMiraiStranger -> contact.sendNudge(contact.nudge())
-            }
-        } else {
-            // 没有target
-            val event = currentCoroutineContext()[EventProcessingContext]?.event
-            if (event is MiraiSimbotEvent<*>) {
-                when (val nativeEvent = event.originalEvent) {
-                    is OriginalMiraiGroupMemberEvent -> nativeEvent.member.sendNudge(nativeEvent.member.nudge())
-                    is OriginalMiraiGroupMessageEvent -> nativeEvent.group.sendNudge(nativeEvent.sender.nudge())
-                    is OriginalMiraiGroupEvent -> nativeEvent.group.sendNudge(nativeEvent.group.bot.nudge())
-                    is OriginalMiraiFriendEvent -> nativeEvent.friend.sendNudge(nativeEvent.friend.nudge())
-                    is StrangerEvent -> nativeEvent.stranger.sendNudge(nativeEvent.stranger.nudge())
-                }
-            } else {
-                // 发送nudge自己
-                contact.sendNudge(contact.bot.nudge())
-            }
-        }
+        sendNudge(contact, target)
         return EmptySingleMessage
     }
 
@@ -92,9 +114,92 @@ public data class MiraiNudge constructor(
 }
 
 
-@SerialName("mirai.nudgeMessage")
+/**
+ * Mirai事件中所接收到的戳一戳事件的信息。
+ *
+ * 与 [MiraiNudge] 类似，此消息会立即发送，不会计入消息列表中。
+ *
+ * @see net.mamoe.mirai.event.events.NudgeEvent
+ */
+@SerialName("mirai.receivedNudge")
 @Serializable
-public class MiraiNudgeMessage {
-    // TODO
+public data class MiraiReceivedNudge @InternalSimbotApi constructor(
+    /**
+     * 戳一戳发起人ID。
+     *
+     * 原类型为 [net.mamoe.mirai.contact.UserOrBot].
+     */
+    public val from: LongID, // UserOrBot
 
+    /**
+     * 戳一戳目标, 可能与 [from] 相同.
+     * 原类型为 [net.mamoe.mirai.contact.UserOrBot].
+     */
+    public val target: LongID, // UserOrBot
+
+    /**
+     * > 可能为 \[Group], \[Stranger], \[Friend], \[Member].
+     * @see SubjectType
+     */
+    public val subjectType: SubjectType,
+    /**
+     * 消息语境ID。
+     * 原类型为 [net.mamoe.mirai.contact.Contact].
+     */
+    public val subject: LongID,
+
+    public val action: String,
+    public val suffix: String,
+) : OriginalMiraiComputableSimbotMessage<MiraiReceivedNudge> {
+
+    /**
+     * [MiraiReceivedNudge] 中 [subjectType] 所使用的可能值。
+     */
+    @Serializable
+    public enum class SubjectType {
+        GROUP, STRANGER, FRIEND, MEMBER
+    }
+
+    /**
+     * 立即发送戳一戳消息，并返回一个 [EmptySingleMessage].
+     */
+    @OptIn(InternalApi::class)
+    @JvmSynthetic
+    override suspend fun originalMiraiMessage(contact: Contact): OriginalMiraiMessage {
+        sendNudge(contact, target)
+        return EmptySingleMessage
+    }
+
+    override val key: Message.Key<MiraiReceivedNudge>
+        get() = Key
+
+    public companion object Key : Message.Key<MiraiReceivedNudge> {
+        override fun safeCast(value: Any): MiraiReceivedNudge? = doSafeCast(value)
+    }
+}
+
+
+/**
+ * 将mirai的 [NudgeEvent] 转化为 [MiraiReceivedNudge] 消息对象。
+ */
+@OptIn(InternalSimbotApi::class)
+public fun NudgeEvent.toMessage(): MiraiReceivedNudge {
+    return MiraiReceivedNudge(
+        from.id.ID,
+        target.id.ID,
+        subject.toSubjectType(),
+        subject.id.ID,
+        action,
+        suffix
+    )
+}
+
+private fun Contact.toSubjectType(): SubjectType {
+    return when (this) {
+        is OriginalMiraiGroup -> SubjectType.GROUP
+        is OriginalMiraiStranger -> SubjectType.STRANGER
+        is OriginalMiraiFriend -> SubjectType.FRIEND
+        is OriginalMiraiMember -> SubjectType.MEMBER
+        else -> throw NoSuchElementException("Subject type by contact $this")
+    }
 }
