b9537f017ff460dc1b85e77b001dcf4c1991d563
ForteScarlet <ForteScarlet@163.com>
2022-05-17 11:24:52 +0800

    fac update

--- simbot-component-mirai-boot/src/main/kotlin/love/forte/simboot/component/mirai/MiraiBotRegistrarFactory.kt
+++ simbot-component-mirai-boot/src/main/kotlin/love/forte/simboot/component/mirai/MiraiBotRegistrarFactory.kt
@@ -17,17 +17,13 @@
 
 package love.forte.simboot.component.mirai
 
-import love.forte.simboot.factory.*
-import love.forte.simbot.*
-import love.forte.simbot.component.mirai.*
-import love.forte.simbot.event.*
-import javax.inject.*
+import love.forte.simboot.factory.BotRegistrarFactory
+import love.forte.simbot.BotRegistrar
+import love.forte.simbot.component.mirai.miraiBotManager
+import love.forte.simbot.event.EventProcessor
 
-/**
- * Mirai bot注册器工厂。
- * @author ForteScarlet
- */
-@Named("miraiBotRegistrarFactory")
+
+@Deprecated("No longer use")
 public class MiraiBotRegistrarFactory : BotRegistrarFactory {
     override fun invoke(processor: EventProcessor): BotRegistrar {
         return miraiBotManager(processor)
