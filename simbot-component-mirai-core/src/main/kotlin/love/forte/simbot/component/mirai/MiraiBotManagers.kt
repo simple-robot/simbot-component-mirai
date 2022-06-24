d7cc37b58902da833e3d3670bd588ee76bd0c476
ForteScarlet <ForteScarlet@163.com>
2022-06-24 21:33:38 +0800

    提供更多获取 MiraiBotManager 的扩展函数

--- simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/MiraiBotManagers.kt
+++ simbot-component-mirai-core/src/main/kotlin/love/forte/simbot/component/mirai/MiraiBotManagers.kt
@@ -12,13 +12,16 @@
  *  https://www.gnu.org/licenses/gpl-3.0-standalone.html
  *  https://www.gnu.org/licenses/lgpl-3.0-standalone.html
  *
- *
  */
 
 package love.forte.simbot.component.mirai
 
+import love.forte.simbot.FragileSimbotApi
+import love.forte.simbot.OriginBotManager
+import love.forte.simbot.application.Application
 import love.forte.simbot.application.ApplicationBuilder
 import love.forte.simbot.application.BotRegistrar
+import love.forte.simbot.application.EventProvider
 
 
 /**
@@ -131,3 +134,118 @@ public inline fun BotRegistrar.miraiIfSupport(block: MiraiBotManager.() -> Unit)
     } ?: throw NoSuchElementException("No event provider of type [MiraiBotManager] in providers: $providers")
     miraiBotManager.block()
 }
+
+
+// region manager获取扩展
+/**
+ * 通过 [OriginBotManager] 获取所有的 [MiraiBotManager]。
+ *
+ * @see miraiBotManagers
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+@Deprecated("Use miraiBotManagers()", ReplaceWith("miraiBotManagers()"))
+public inline fun miraiComponents(): List<MiraiBotManager> = miraiBotManagers()
+
+/**
+ * 从 [OriginBotManager] 获取第一个 [MiraiComponent]。
+ *
+ * @throws [NoSuchElementException] 如果找不到的话
+ *
+ * @see firstMiraiBotManager
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+@Deprecated("Use firstMiraiBotManager()", ReplaceWith("firstMiraiBotManager()"))
+public inline fun firstMiraiComponent(): MiraiBotManager = firstMiraiBotManager()
+
+/**
+ * 从 [OriginBotManager] 获取第一个 [MiraiComponent]。
+ *
+ * 如果找不到则返回null。
+ *
+ * @see firstMiraiBotManagerOrNull
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+@Deprecated("Use firstMiraiBotManagerOrNull()", ReplaceWith("firstMiraiBotManagerOrNull()"))
+public inline fun firstMiraiComponentOrNull(): MiraiBotManager? = firstMiraiBotManagerOrNull()
+
+/**
+ * 通过 [OriginBotManager] 获取所有的 [MiraiBotManager]。
+ *
+ * @see OriginBotManager
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+public inline fun miraiBotManagers(): List<MiraiBotManager> = OriginBotManager.filterIsInstance<MiraiBotManager>()
+
+/**
+ * 从 [OriginBotManager] 获取第一个 [MiraiComponent]。
+ *
+ * @throws [NoSuchElementException] 如果找不到的话
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+public inline fun firstMiraiBotManager(): MiraiBotManager =
+    OriginBotManager.first { it is MiraiBotManager } as MiraiBotManager
+
+/**
+ * 从 [OriginBotManager] 获取第一个 [MiraiComponent]。
+ *
+ * 如果找不到则返回null。
+ */
+@FragileSimbotApi
+@Suppress("NOTHING_TO_INLINE")
+public inline fun firstMiraiBotManagerOrNull(): MiraiBotManager? =
+    OriginBotManager.firstOrNull { it is MiraiBotManager } as MiraiBotManager?
+
+
+/**
+ * 获取其中为 [MiraiBotManager] 的管理器。
+ */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun Iterable<EventProvider>.filterIsMiraiBotManagers(): List<MiraiBotManager> =
+    filterIsInstance<MiraiBotManager>()
+
+
+/**
+ * 过滤获取其中为 [MiraiBotManager] 的管理器。
+ */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun Sequence<EventProvider>.filterIsMiraiBotManagers(): Sequence<MiraiBotManager> =
+    filterIsInstance<MiraiBotManager>()
+/**
+ * 获取其中第一个为 [MiraiBotManager] 的管理器。
+ * 如果找不到则抛出 [NoSuchElementException]。
+ *
+ * @throws NoSuchElementException 如果找不到
+ */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun Iterable<EventProvider>.firstMiraiBotManager(): MiraiBotManager = first { it is MiraiBotManager } as MiraiBotManager
+
+
+/**
+ * 过滤获取其中第一个为 [MiraiBotManager] 的管理器。
+ * 如果找不到则得到null。
+ */
+@Suppress("NOTHING_TO_INLINE")
+public inline fun Sequence<EventProvider>.firstMiraiBotManagerOrNull(): MiraiBotManager? = firstOrNull { it is MiraiBotManager } as MiraiBotManager?
+
+
+/**
+ * 得到 [OriginBotManager] 中的所有 mirai 组件。
+ *
+ * @see OriginBotManager
+ */
+@FragileSimbotApi
+public inline val OriginBotManager.miraiBotManagers: List<MiraiBotManager> get() = filterIsMiraiBotManagers()
+
+
+/**
+ * 从 [Application.providers] 中寻找所有的 [MiraiBotManager].
+ *
+ */
+public inline val Application.miraiBotManagers: List<MiraiBotManager> get() = this.providers.filterIsMiraiBotManagers()
+
+// endregion
